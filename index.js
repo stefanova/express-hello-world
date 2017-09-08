67faa9ade51352e74f03cc247707c582dd2212c3
Karolina Kreft <kar.kreft@gmail.com>
2017-09-08 16:05:07 +0200

    Middleware for all requests

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-
-
 var express = require('express');
-//
-
-
 var app = express();
 
+var things = require('./things.js');
 
 
 
-app.get('/', function(req, res){
-    // request object(req) represents the HTTP request
-    // and has properties for the request query string,
-    // parameters, body, HTTP headers, etc.
-
-
-    //response object represents the HTTP response that
-    // the Express app sends when it receives an HTTP request
-
-    res.send("Hello World!");
-    });
-
-
+//both index.js and things.js should be in same directory
+app.use('/things', things);
 
 app.listen(3000);
---
+++ things.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+
+//Middleware
+router.use('/things',function(req, res, next){
+    console.log("2 A new request received at " + Date.now());
+
+    next();
+});
+
+
+//Middleware for all requests - WORKS
+router.use(function(req, res, next){
+    console.log("A new request received at " + Date.now());
+    res.on("finish", function () {
+    });
+
+    next();
+});
+
+router.get('/', function(req, res){
+    res.send('GET route on things.');
+});
+
+router.post('/', function(req, res){
+    res.send('POST route on things.');
+});
+
+router.get('/:id', function(req, res){
+    res.send("The id you specified is " + req.params.id);
+});
+
+// regEx --> regular expressions
+// nie dziala :(
+router.get('/things/:id([0-9]{5})', function(req, res){
+    res.send('id: ' + req.params.id);
+});
+
+//customised 404 error --> should be placed as a last route
+router.get('*', function (req, res) {
+    res.send("Sorry, this is an invalid URL");
+});
+
+module.exports = router;
